cc42137c197c45fee19a335c9def1801817afedc
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-06-19 13:40:30 +0300

    examples "addEventListener"

--- js/script.js
+++ js/script.js
@@ -43,47 +43,65 @@
 // console.log(refs.text.getAttribute('data-id'));
 // console.log(refs.text.getAttribute('id'));
 
-const cars = [
-  {
-  id: 1,  
-  car: 'Honda',
-  type: 'Civic',
-  price: 12000,
-  img: '#'
-},
-{
-  id: 2,
-  car: 'Audi',
-  type: 'Q7',
-  price: 40000,
-  img: '#'
-},
-{
-  id: 3,
-  car: 'Honda',
-  type: 'Accord',
-  price: 20000,
-  img: '#'
-},
-{
-  id: 4,
-  car: 'BMW',
-  type: '5 siries',
-  price: 9000,
-  img: '#'
-},
-{
-  id: 5,
-  car: 'Volvo',
-  type: 'XC60',
-  price: 7000,
-  img: '#'
-},
-]
+// const cars = [
+//   {
+//   id: 1,  
+//   car: 'Honda',
+//   type: 'Civic',
+//   price: 12000,
+//   img: '#'
+// },
+// {
+//   id: 2,
+//   car: 'Audi',
+//   type: 'Q7',
+//   price: 40000,
+//   img: '#'
+// },
+// {
+//   id: 3,
+//   car: 'Honda',
+//   type: 'Accord',
+//   price: 20000,
+//   img: '#'
+// },
+// {
+//   id: 4,
+//   car: 'BMW',
+//   type: '5 siries',
+//   price: 9000,
+//   img: '#'
+// },
+// {
+//   id: 5,
+//   car: 'Volvo',
+//   type: 'XC60',
+//   price: 7000,
+//   img: '#'
+// },
+// ]
 
-const listRef = document.querySelector('.js-list');
-// метод MAP
-// const markup = cars.map(({id,car,type,price,img}) => 
+// const listRef = document.querySelector('.js-list');
+// const formRef = document.querySelector('.js-search');
+
+// formRef.addEventListener('submit',onSearch);
+
+// function onSearch(event){
+//   event.preventDefault();
+//   const {searchQuery, searchKey} = event.currentTarget.elements
+
+//   // const currentCar = cars.filter(item => item[searchKey.value].toLowerCase() === searchQuery.value.trim().toLowerCase());
+//   const currentCar = cars.filter(item => item[searchKey.value].toLowerCase().includes(searchQuery.value.trim().toLowerCase()));
+//   console.log(currentCar);
+
+//   if(currentCar.length){
+//     listRef.innerHTML = creatMarkup(currentCar);
+//   }else{
+//     listRef.innerHTML = ` <li><img src="https://previews.123rf.com/images/kaymosk/kaymosk1804/kaymosk180400006/100130939-error-404-page-not-found-error-with-glitch-effect-on-screen-vector-illustration-for-your-design.jpg" alt="404" width="300px"></img></li>`
+//   }
+// }
+// function creatMarkup(arr){
+//   return arr.map(({id,car,type,price,img}) => 
 //   `<li data-id=${id} >
 //   <img src="${img}" alt="${type}" width=300px">
 //   <h2>Марка:${car} </h2>
@@ -91,31 +109,89 @@ const listRef = document.querySelector('.js-list');
 //   <p>Ціна: ${price}</p>
 //   </li>`
 // ).join('');
+// }
+
+// listRef.insertAdjacentHTML('beforeend', creatMarkup(cars));
+// метод MAP
+
 
 // метод Reduce
- const markup = cars.reduce((acc,{id,car,type,price,img}) => 
-  acc + `<li data-id=${id} >
-  <img src="${img}" alt="${type}" width=300px">
-  <h2>Марка:${car} </h2>
-  <h2>Модель: ${type}</h2>
-  <p>Ціна: ${price}</p>
-  </li>`, ''
-)
-
-
-listRef.style.listStyle = 'none';
-listRef.style.display = 'flex';
-listRef.style.flexWrap = 'wrap';
-listRef.style.justifyContent = 'space-between';
-
-
-
+//  const markup = cars.reduce((acc,{id,car,type,price,img}) => 
+//   acc + `<li data-id=${id} >
+//   <img src="${img}" alt="${type}" width=300px">
+//   <h2>Марка:${car} </h2>
+//   <h2>Модель: ${type}</h2>
+//   <p>Ціна: ${price}</p>
+//   </li>`, ''
+// )
+// listRef.style.listStyle = 'none';
+// listRef.style.display = 'flex';
+// listRef.style.flexWrap = 'wrap';
+// listRef.style.justifyContent = 'space-between';
 // console.log(markup);
-listRef.insertAdjacentHTML('beforeend', markup);
 
-const items = document.querySelectorAll('li');
-items.forEach(item => item.style.width = '300px')
-console.log(items);
+
+// const items = document.querySelectorAll('li');
+// items.forEach(item => item.style.width = '300px')
+// console.log(items);
+
+// ============== input===========================
+
+// const input = document.querySelector('.js-input');
+// const title = document.querySelector('.js-title');
+
+// input.addEventListener('input', onSearchInput);
+// input.addEventListener('change', onSearchChange);
+
+// function onSearchInput(evt){
+//   console.log(evt.currentTarget.value);
+//   title.textContent = evt.currentTarget.value;
+// }
+// function onSearchChange(){
+//   alert('Повернісь в інпут');
+// }
+
+// ==================click=============
+
+const obj = {
+  name: 'Iphone',
+  price: '1200$',
+  color: 'black'
+}
+const clickMe = document.querySelector('.js-click');
+clickMe.addEventListener('click',onClickMe.bind(obj));
+
+let counter = 1;
+function onClickMe(evt){
+  console.log(evt);
+  console.log(this);
+  const changeValue = 50*counter;
+  counter += 1;
+  evt.currentTarget.style.margin = `${changeValue}px ${changeValue}px`;
+  // document.addEventListener('keydown', onKey)
+}
+
+// ==============focus=========
+//===========keydown і keyup============
+
+// document.addEventListener('keydown',onKey)
+
+// function onKey(evt){
+//   console.log(evt);
+//   if(evt.code === 'Escape'){
+//     document.body.classList.toggle('test');
+//     document.removeEventListener('keydown', onKey);
+//   }
+// }
+
+// =============================
+
+// document.addEventListener('mousemove', onMove);
+
+// function onMove (evt){
+//   console.log(evt);
+// }
+
 
 
 
