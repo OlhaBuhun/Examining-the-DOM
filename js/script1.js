f41d4fa98a43e0570b9c639f7cf74da174668a20
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-07-06 14:47:22 +0300

    education DOM

--- js/script1.js
+++ js/script1.js
@@ -61,39 +61,92 @@
 // Открыть ы закрыть по кнопке, закрыть по клику на бекдроп, закрыть по ESC
 // В CSS есть класс show-modal, который необходимо добавить на body при открытии модалки
 
-const refs = {
-  openModalBtn: document.querySelector('[data-action = "open-modal"]'),
-  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
-  backdrop: document.querySelector('.js-backdrop'),
-};
+// const refs = {
+//   openModalBtn: document.querySelector('[data-action = "open-modal"]'),
+//   closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+//   backdrop: document.querySelector('.js-backdrop'),
+// };
 
-refs.openModalBtn.addEventListener('click', onOpenModal);
-refs.closeModalBtn.addEventListener('click', onCloseModal);
-refs.backdrop.addEventListener('click', onBackdropClick);
+// refs.openModalBtn.addEventListener('click', onOpenModal);
+// refs.closeModalBtn.addEventListener('click', onCloseModal);
+// refs.backdrop.addEventListener('click', onBackdropClick);
 
-function onOpenModal(){
-  window.addEventListener('keydown', onEscKeyPress);
-  document.body.classList.add('show-modal');
-}
-function onCloseModal(){
-  window.removeEventListener('keydown', onEscKeyPress);
-  document.body.classList.remove('show-modal');
-}
-function onBackdropClick(event){
+// function onOpenModal(){
+//   window.addEventListener('keydown', onEscKeyPress);
+//   document.body.classList.add('show-modal');
+// }
+// function onCloseModal(){
+//   window.removeEventListener('keydown', onEscKeyPress);
+//   document.body.classList.remove('show-modal');
+// }
+// function onBackdropClick(event){
+//   // console.log(event.currentTarget);
+//   // console.log(event.target);
+
+//   if(event.currentTarget === event.target){
+//     console.log('Клик по backdrop');
+//     onCloseModal();
+//   }
+// }
+
+// function onEscKeyPress(event){
+//   console.log(event.code);
+//   if(event.code === 'Escape'){
+//     onCloseModal();
+//   }
+// }
+
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+
+
+// parent.addEventListener("click", (event) => {
+//   console.log("Parent click handler");
+//   console.log(event.target);
+//   console.log(event.currentTarget);
+// });
+
+// child.addEventListener("click", (event) => {
+//  console.log("Child click handler");
+//  console.log(event.target);
+//   console.log(event.currentTarget);
+// });
+
+// descendant.addEventListener("click", (event) => {
+//   console.log("Descendant click handler");
+//   console.log(event.target);
+//   console.log(event.currentTarget);
+// });
+
+// ========
+const container = document.querySelector('.js-container');
+
+container.addEventListener('click', onClick);
+
+function onClick(event){
+  // console.log(event.target.nodeName);
   // console.log(event.currentTarget);
-  // console.log(event.target);
 
-  if(event.currentTarget === event.target){
-    console.log('Клик по backdrop');
-    onCloseModal();
+  if(event.target.nodeName !== 'BUTTON'){
+    return;
   }
-}
+  console.log(event.target.textContent);
 
-function onEscKeyPress(event){
-  console.log(event.code);
-  if(event.code === 'Escape'){
-    onCloseModal();
-  }
 }
 
 
+const addBtn = document.querySelector('.js-add-btn');
+let labelCounter = 6;
+
+addBtn.addEventListener('click', onAddBtn);
+
+function onAddBtn(){
+  const btn = document.createElement('button');
+  btn.textContent = `Кнопка ${labelCounter}`;
+  btn.type = 'button';
+
+  container.appendChild(btn);
+  labelCounter += 1;
+
+}
